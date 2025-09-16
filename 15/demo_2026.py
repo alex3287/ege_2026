e21bf3428e739e002ee6e47c8fca2dc014a45616
alex3287 <alex3287@bk.ru>
2025-09-16 21:05:28 +0300

    KIM-15 demo_26

---
+++ 15/demo_2026.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def F(x):
+    P = 25 <= x <= 64
+    Q = 40 <= x <= 115
+    A = a1 <= x <= a2
+    return (P) <= ((Q and (not A)) <= (not P))
+
+Ox = [i/4 for i in range(24*4, 116*4+1)]
+arr = []
+
+for a1, a2 in combinations(Ox, 2):
+    if all(F(x)==1 for x in Ox):
+        arr.append(a2-a1)
+
+print(arr)
+print(min(arr), len(arr))
